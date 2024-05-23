b2d5ecdeedf205fcfe9c48417394a30ab4018f6f
choiminkyung <choialsrud2002@gmail.com>
2024-05-23 09:40:10 +0900

    Initial Commit

---
+++ 0523/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0523
+//
+//  Created by 최민경 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
