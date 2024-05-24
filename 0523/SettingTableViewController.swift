cf0ac17ec266954420591b2a11e4ac6876c6040b
choiminkyung <choialsrud2002@gmail.com>
2024-05-24 09:33:56 +0900

    주석 삭제

--- 0523/SettingTableViewController.swift
+++ 0523/SettingTableViewController.swift
@@ -11,7 +11,6 @@ class SettingTableViewController: UITableViewController {
     
     var headerList = ["전체설정", "공지사항", "기타"]
     
-//    var list = [[ "공지사항", "실험실", "버전 정보"], [ "개인 설정", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
     var list = [ "공지사항", "실험실", "버전 정보"]
     var list1 = [ "개인 설정", "알림", "채팅", "멀티프로필"]
     var list2 = ["고객센터/도움말"]
@@ -45,19 +44,14 @@ class SettingTableViewController: UITableViewController {
         print(#function)
         let cell = tableView.dequeueReusableCell(withIdentifier: "Setting")!
         
-//        cell.textLabel?.text = list[0][indexPath.row]
         if indexPath.section == 0 {
             cell.textLabel?.text = list[indexPath.row]
         } else if indexPath.section == 1 {
             cell.textLabel?.text = list1[indexPath.row]
         } else if indexPath.section == 2 {
             cell.textLabel?.text = list2[0]
-//            print("오류")
         }
 
-          
-        
-//        cell.textLabel?.textColor = .black
         cell.textLabel?.font = .boldSystemFont(ofSize: 16)
         
         return cell
@@ -70,24 +64,4 @@ class SettingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return headerList[section]
     }
-    
-//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let headerView = UIView()
-//        
-//        
-//        let label = UILabel()
-//        label.text = headerList[section]
-//        
-//        
-//        label.frame = CGRect(x: 20, y: 0, width: tableView.frame.width, height: 15)
-//        label.textColor = .lightGray
-//        label.font = .systemFont(ofSize: 15)
-//        headerView.addSubview(label)
-//        return headerView
-//    }
-    
-    
-    
-    
-    
 }
